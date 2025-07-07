8d4e081854771281c2a28e8a10d60463beda2d1f
onponitcrm <jamesp732019@gmail.com>
2025-07-06 21:59:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function triggerBreak() {
+  document.getElementById("track").play();
+  
   document.querySelector('.left-hand').classList.add('break');
   document.querySelector('.right-hand').classList.add('break');
   document.querySelector('.shards').classList.add('break');
